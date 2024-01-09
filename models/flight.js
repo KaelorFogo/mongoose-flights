c83383bc6f5ca60093d3120270c2044c1aece638
KaelorFogo <kfogo19@gmail.com>
2024-01-08 18:12:46 -0700

    Part 1

---
+++ models/flight.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new mongoose.Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United'],
+    required: true
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 1000
+  },
+  departs: {
+    type: Date,
+    default: function() {
+      const oneYearFromNow = new Date();
+      oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+      return oneYearFromNow;
+    }
+  }
+});
+
+module.exports = mongoose.model('Flights', flightSchema);
